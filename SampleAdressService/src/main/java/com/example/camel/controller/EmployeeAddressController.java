2d480bb5078f237a227a8cb46ab34fd1368d9cf8
Lakshmi N Prasad Kakumani <kln550@users.noreply.github.com>
2020-11-02 21:00:00 -0500

    Refactoring to use REST API.

--- SampleAdressService/src/main/java/com/example/camel/controller/EmployeeAddressController.java
+++ SampleAdressService/src/main/java/com/example/camel/controller/EmployeeAddressController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeAddressController {
 
-	@RequestMapping(value = "/employees/{id}/address", method = RequestMethod.GET)
+	@RequestMapping(value = "/emp-address/{id}", method = RequestMethod.GET)
 	public EmployeeDetails getEmployeeDetails(@PathVariable String id) {
 		return new EmployeeDetails(id, "Stree1, city-1, MA, USA, 11111");
 	}
